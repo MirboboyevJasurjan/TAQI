7b22fe7be8ca5b878eccf7046f69597cfd4a3e5b
StiLKnoT <stilknot@gmail.com>
2022-06-27 23:10:09 +0500

    ss

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import App from './App';
 
 
 ReactDOM.render(
-  <Suspense fallback={<h1>Loading...</h1>}>
+  
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>  
-  </React.StrictMode>
-  </Suspense>,
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>  
+    </Suspense>
+  </React.StrictMode>,
+
   document.getElementById('root')
 );
 
